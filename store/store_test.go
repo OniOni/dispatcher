8aeed3880b8fbe1cae9235d5266cb2797059dc7a
OniOni <mathieu.c.sabourin@gmail.com>
2015-03-08 23:53:33 -0700

    Update tests.

--- store/store_test.go
+++ store/store_test.go
@@ -1,7 +1,6 @@
 package store
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -13,19 +12,36 @@ func Test_AddSubscriber(t *testing.T) {
 	defer store.Close()
 	store.db.Clear()
 
-	fmt.Println("Store db", store.db)
+	store.AddSubsriber("lol", "one")
+	if error != nil {
+		t.Error("Error setting value.")
+	}
+
+	subs, error := store.GetSubscribers("lol")
+	if error != nil {
+		t.Error("Error fecthing value.")
+	}
+
+	if subs[0] != "one" {
+		t.Error("Value was not as expected.", subs[0])
+	}
+}
+
+func Test_IsSubscribed(t *testing.T) {
+	store, error := NewStore()
+	if error != nil {
+		t.Error("Error creating store.")
+	}
+	defer store.Close()
+	store.db.Clear()
 
 	store.AddSubsriber("lol", "one")
 	if error != nil {
 		t.Error("Error setting value.")
 	}
 
-	value, error := store.GetSubscribers("lol")
-	if error != nil {
-		t.Error("Error fecthing value.")
-	}
-
-	if value != "one" {
-		t.Error("Value was not as expected.", value)
+	is, error := store.IsSubscribed("lol", "one")
+	if !is {
+		t.Error("Should be subscirbed.")
 	}
 }

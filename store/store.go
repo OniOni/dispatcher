8b1a74b11983b95d7977cfdaf5f407d879fa38f2
OniOni <mathieu.c.sabourin@gmail.com>
2015-03-15 17:39:05 -0700

    Add HasKey method.

--- store/store.go
+++ store/store.go
@@ -7,19 +7,33 @@ import (
 
 type Store struct {
 	db *kc.DB;
+	keys []string;
 }
 
-func (s Store) Open() (error, Store) {
+func (s *Store) Open() (error, *Store) {
 	db, err := kc.Open("/tmp/cache.kch", kc.WRITE)
 	s.db = db
 	return err, s;
 }
 
-func (s Store) Close(){
+func (s *Store) Close(){
 	s.db.Close()
 }
 
-func (s Store) GetSubscribers(key string) ([]string, error){
+//func (s Store) GetKeys() ([]string, error) {
+//}
+
+func (s *Store) HasKey(key string) (bool) {
+	for _, str := range s.keys {
+		if str == key {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+func (s *Store) GetSubscribers(key string) ([]string, error){
 	str, error := s.db.Get(key);
 	if error != nil {
 		return nil, error
@@ -28,11 +42,15 @@ func (s Store) GetSubscribers(key string) ([]string, error){
 	return strings.Split(str, ";")[1:], nil
 }
 
-func (s Store) AddSubsriber(key, value string) error{
+func (s *Store) AddSubsriber(key, value string) error{
+	if !s.HasKey(key) {
+		s.keys = append(s.keys, key)
+	}
+
 	return s.db.Append(key, ";" + value);
 }
 
-func (s Store) IsSubscribed(key, value string) (bool, error){
+func (s *Store) IsSubscribed(key, value string) (bool, error){
 	str, error := s.db.Get(key)
 	if  error != nil {
 		return false, error
@@ -41,9 +59,11 @@ func (s Store) IsSubscribed(key, value string) (bool, error){
 	return strings.Contains(str, value), nil
 }
 
-func NewStore() (Store, error) {
+func NewStore() (*Store, error) {
 	s_ptr := new(Store)
 	err, s := s_ptr.Open()
+
+	s.keys = make([]string, 0)
 
 	return s, err
 }
